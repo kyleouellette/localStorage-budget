d437cbcc605713065c9f607d1827140071cfe24e
Kyle <kouellette20@gmail.com>
2012-01-28 20:40:03 -0500

    added style and more functionality. Still needs a lot but hey, it's progress

--- _js/main.js
+++ _js/main.js
@@ -1,14 +1,33 @@
 (function($, b, undefined){
 	
-	var console = window.console || {'log' : function(a){return a}};
+	var console = window.console || {'log' : function(a){return a}},
+		entries = $('#entries ul'),
+		populate;
 	console.log(b);
+
+	populate = function(){
+		var li,
+			i = 0;
+
+		b.update();
+		for(; i < b.saved.length; i++){
+			$('<li />').html(b.saved[i].name + '<span>$'+b.saved[i].price+'</span>').attr({
+				'data-number' : i
+			}).appendTo(entries).bind('click', function(e){
+				console.log($(this).attr('data-number'));
+			});
+		}
+	};
+
+	populate();
 
 	$('#create').bind('submit', function(){
 		var t = $(this),
 			c = t.children();
 
 		b.create(c[0].value,c[1].value);
-		b.update();
+		
+		populate();
 
 		for(var i = 0; i < c.length; i++){
 			c[i].value = '';
@@ -16,6 +35,11 @@
 
 		return false;
 	});
+
+	$('h3').bind('click', function(){
+		$('#add-form').slideToggle();
+	});
+
 
 
 })(jQuery, budget);
